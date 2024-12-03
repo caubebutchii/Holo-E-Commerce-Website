e86b20a7a67c9fe212465e63bf97ec73075464f7
caubebutchii <caubebutchi2k3@gmail.com>
2024-12-03 20:15:31 +0700

    fix lỗi quăng error ko cần thiết

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -3,7 +3,7 @@ import AppNavigator from '../src/navigation/AppNavigator';
 import { UserProvider } from '../src/context/UserContext';
 import { LogBox } from 'react-native'; 
 export default function App() {
-  LogBox.ignoreLogs([ '[Reanimated] Reading from `value` during component render.', ]);
+  LogBox.ignoreLogs([ '[Reanimated] Reading from `value` during component render.', 'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead. [Component Stack]']);
   return (
 
     // AppNavigator sẽ được bọc trong UserProvider để sử dụng context
